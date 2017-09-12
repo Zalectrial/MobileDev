835abc072372bba3fda1ef902449e32b5ffd648c
Zalectrial <vandeventerrobyn@gmail.com>
2017-09-12 21:22:09 +1000

    Add temperature conversion code.

--- app/src/main/java/com/example/zalectrial/mobiledev/TemperatureActivity.java
+++ app/src/main/java/com/example/zalectrial/mobiledev/TemperatureActivity.java
@@ -2,6 +2,10 @@ package com.example.zalectrial.mobiledev;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TemperatureActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class TemperatureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature);
+
+        final Button convertButton = (Button) findViewById(R.id.temp_convert_button);
+        convertButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                EditText celsius = (EditText) findViewById(R.id.temp_celsius);
+                TextView convertedTemp = (TextView) findViewById(R.id.converted_temp);
+
+                convertedTemp.setText(convertTemperature(String.valueOf(celsius.getText())));
+            }
+        });
+    }
+
+    private String convertTemperature(String celsius) {
+
+        try {
+            double c = Double.parseDouble(celsius);
+            double f = c * (9.0/5.0) + 32.0;
+            return String.format("%3.2f", f);
+        }
+        catch (NumberFormatException nfe) {
+            return "ERR";
+        }
     }
 }
