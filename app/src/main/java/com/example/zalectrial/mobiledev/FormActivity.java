29ccf60f0840ffb75d9190cb482cc176012d52a3
Zalectrial <vandeventerrobyn@gmail.com>
2017-09-25 19:56:23 +1000

    Display hints for the fields on FormActivity.

--- app/src/main/java/com/example/zalectrial/mobiledev/FormActivity.java
+++ app/src/main/java/com/example/zalectrial/mobiledev/FormActivity.java
@@ -19,7 +19,19 @@ public class FormActivity extends AppCompatActivity {
         EditText imageName = (EditText) findViewById(R.id.image_name);
         imageName.setHint(image.name);
 
+        EditText imageLocation = (EditText) findViewById(R.id.image_location);
+        imageLocation.setHint(image.URL == null ? "URL" : image.URL);
+
+        EditText imageKeywords = (EditText) findViewById(R.id.image_keywords);
+        imageKeywords.setHint(image.name);
+
         EditText imageDate = (EditText) findViewById(R.id.image_date);
         imageDate.setHint(format.format(image.date));
+
+        EditText imageOwner = (EditText) findViewById(R.id.image_owner);
+        imageOwner.setHint(image.email == null ? "E-mail" : image.email);
+
+        EditText imageRating = (EditText) findViewById(R.id.image_rating);
+        imageRating.setHint(image.rating == 0 ? "3.5" : String.valueOf(image.rating));
     }
 }
