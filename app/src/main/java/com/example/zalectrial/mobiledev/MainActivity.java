5679d56942559f896a689920bf618feab8548842
Zalectrial <vandeventerrobyn@gmail.com>
2017-10-22 22:37:31 +1100

    Present updated image information on MainAcivity after resuming activity

--- app/src/main/java/com/example/zalectrial/mobiledev/MainActivity.java
+++ app/src/main/java/com/example/zalectrial/mobiledev/MainActivity.java
@@ -13,6 +13,15 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     public static Image[] images;
+    TextView burgerName;
+    TextView burgerDate;
+    TextView cakeName;
+    TextView cakeDate;
+    TextView pizzaName;
+    TextView pizzaDate;
+    TextView smashedAvoName;
+    TextView smashedAvoDate;
+    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,42 +29,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         images = new Image[4];
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
         // Get all ImageViews from the layout
         ImageView burger = (ImageView) findViewById(R.id.burger);
         Image burgerImage = new Image("Burger", Calendar.getInstance().getTimeInMillis());
         images[0] = burgerImage;
-        TextView burgerName = (TextView) findViewById(R.id.burger_name);
+        burgerName = (TextView) findViewById(R.id.burger_name);
+        burgerDate = (TextView) findViewById(R.id.burger_date);
         burgerName.setText(images[0].name);
-        TextView burgerDate = (TextView) findViewById(R.id.burger_date);
         burgerDate.setText(format.format(images[0].date));
         burger.setTag(0);
 
         ImageView cake = (ImageView) findViewById(R.id.cake);
         Image cakeImage = new Image("Cake", Calendar.getInstance().getTimeInMillis());
         images[1] = cakeImage;
-        TextView cakeName = (TextView) findViewById(R.id.cake_name);
+        cakeName = (TextView) findViewById(R.id.cake_name);
+        cakeDate = (TextView) findViewById(R.id.cake_date);
         cakeName.setText(images[1].name);
-        TextView cakeDate = (TextView) findViewById(R.id.cake_date);
         cakeDate.setText(format.format(images[1].date));
         cake.setTag(1);
 
         ImageView pizza = (ImageView) findViewById(R.id.pizza);
         Image pizzaImage = new Image("Pizza", Calendar.getInstance().getTimeInMillis());
         images[2] = pizzaImage;
-        TextView pizzaName = (TextView) findViewById(R.id.pizza_name);
+        pizzaName = (TextView) findViewById(R.id.pizza_name);
+        pizzaDate = (TextView) findViewById(R.id.pizza_date);
         pizzaName.setText(images[2].name);
-        TextView pizzaDate = (TextView) findViewById(R.id.pizza_date);
         pizzaDate.setText(format.format(images[2].date));
         pizza.setTag(2);
 
         ImageView smashed_avocado = (ImageView) findViewById(R.id.smashed_avocado);
         Image smashedAvoImage = new Image("Smashed Avocado", Calendar.getInstance().getTimeInMillis());
         images[3] = smashedAvoImage;
-        TextView smashedAvoName = (TextView) findViewById(R.id.smashed_avo_name);
+        smashedAvoName = (TextView) findViewById(R.id.smashed_avo_name);
+        smashedAvoDate = (TextView) findViewById(R.id.smashed_avo_date);
         smashedAvoName.setText(images[3].name);
-        TextView smashedAvoDate = (TextView) findViewById(R.id.smashed_avo_date);
         smashedAvoDate.setText(format.format(images[3].date));
         smashed_avocado.setTag(3);
 
@@ -84,5 +92,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        burgerName.setText(images[0].name);
+        burgerDate.setText(format.format(images[0].date));
+
+        cakeName.setText(images[1].name);
+        cakeDate.setText(format.format(images[1].date));
+
+        pizzaName.setText(images[2].name);
+        pizzaDate.setText(format.format(images[2].date));
+
+        smashedAvoName.setText(images[3].name);
+        smashedAvoDate.setText(format.format(images[3].date));
     }
 }
