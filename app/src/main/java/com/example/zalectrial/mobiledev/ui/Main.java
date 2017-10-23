878cf7c1b25877fc3afbb2139cf29fb6e5ab7a6d
Zalectrial <vandeventerrobyn@gmail.com>
2017-10-23 12:50:56 +1100

    Read in locations and present a dropdown list with all of the places.

--- app/src/main/java/com/example/zalectrial/mobiledev/ui/Main.java
+++ app/src/main/java/com/example/zalectrial/mobiledev/ui/Main.java
@@ -1,6 +1,11 @@
 package com.example.zalectrial.mobiledev.ui;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
@@ -9,14 +14,18 @@ import  com.example.zalectrial.mobiledev.R;
 import  com.example.zalectrial.mobiledev.calc.AstronomicalCalendar;
 import  com.example.zalectrial.mobiledev.calc.GeoLocation;
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.DatePicker.OnDateChangedListener;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Main extends Activity 
 {
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) 
@@ -28,6 +37,16 @@ public class Main extends Activity
 
 	private void initializeUI()
 	{
+        Spinner locationSpinner = (Spinner) findViewById(R.id.locationSpinner);
+        String[] locations;
+        try {
+            locations = readLocations();
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, locations);
+            locationSpinner.setAdapter(adapter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 		DatePicker dp = (DatePicker) findViewById(R.id.datePicker);
 		Calendar cal = Calendar.getInstance();
 		int year = cal.get(Calendar.YEAR);
@@ -63,5 +82,31 @@ public class Main extends Activity
 			updateTime(year, monthOfYear, dayOfMonth);
 		}	
 	};
+
+	private String[] readLocations() throws IOException {
+        String[] locations;
+
+        AssetManager assetManager = this.getAssets();
+        InputStream inputStream = assetManager.open("Locations");
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        ArrayList<GeoLocation> temp = new ArrayList<>();
+
+        String line = bufferedReader.readLine();
+        while (line != null) {
+            String[] splitLine = line.split(",");
+            GeoLocation location = new GeoLocation(splitLine[0], Double.parseDouble(splitLine[1]), Double.parseDouble(splitLine[2]), TimeZone.getTimeZone(splitLine[3]));
+            temp.add(location);
+            line = bufferedReader.readLine();
+        }
+
+        bufferedReader.close();
+        locations = new String[temp.size()];
+        for (int i = 0; i < temp.size(); i++) {
+            locations[i] = temp.get(i).getLocationName();
+        }
+
+        return locations;
+    }
 	
 }
